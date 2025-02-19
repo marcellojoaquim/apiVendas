041b345a8dd19c28581d92e1f3fc14fefac100b2
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-02-18 23:26:28 -0300

    fix db config

--- src/common/infrastructure/env/index.ts
+++ src/common/infrastructure/env/index.ts
@@ -7,13 +7,20 @@ const envSchema = z.object({
     .enum(['development', 'production', 'test'])
     .default('development'),
   PORT: z.coerce.number().default(3333),
-  API_url: z.string().default('http://localhost:3333'),
+  API_URL: z.string().default('http://localhost:3333'),
+  DB_TYPE: z.literal('postgres').default('postgres'),
+  DB_HOST: z.string().default('localhost'),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_SCHEMA: z.string().default('public'),
+  DB_NAME: z.string().default('postgres'),
+  DB_USER: z.string().default('postgres'),
+  DB_PASS: z.string().default('postgres'),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  throw new AppError('Invalid enviroment viriables')
+  throw new AppError('Invalid enviroment variables')
 }
 
 export const env = _env.data
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,15 @@
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`)
-  console.log('API-Docs available at GET /docs')
-})
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server running on port ${env.PORT}`)
+      console.log('API-Docs available at GET /docs')
+    })
+  })
+  .catch(error => {
+    console.error('Error initializing data source: ', error)
+  })
