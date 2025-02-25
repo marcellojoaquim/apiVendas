d522a28ba5cdd1215cec8f3a072fdc320e4e1f92
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-02-24 22:48:25 -0300

    new tests for repository implemented

--- src/common/domain/repositories/in-memory.repository.spec.ts
+++ src/common/domain/repositories/in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'node:crypto';
 import { InMemoryRepository } from './in-memory.repository';
+import { NotFoundError } from '../errors/not-found-error';
 
 type StubModelProps = {
   id: string;
@@ -23,7 +24,7 @@ class StubInMemoryRepository extends InMemoryRepository<StubModelProps> {
       return items;
     }
     return items.filter(item => {
-      item.name.toLowerCase().includes(filter.toLowerCase());
+      return item.name.toLowerCase().includes(filter.toLowerCase());
     });
   }
 }
@@ -51,14 +52,102 @@ describe('InMemoryRepository unit test', () => {
     };
   });
 
-  it('Should create a new Model', () => {
-    const result = sut.create(props);
-    expect(result.name).toStrictEqual(model.name);
-    expect(result.price).toStrictEqual(10);
+  describe('Create', () => {
+    it('Should create a new Model', () => {
+      const result = sut.create(props);
+      expect(result.name).toStrictEqual(model.name);
+      expect(result.price).toStrictEqual(10);
+    });
   });
 
-  it('Should insert a new model', async () => {
-    const result = await sut.insert(model);
-    expect(result).toStrictEqual(sut.items[0]);
+  describe('Insert', () => {
+    it('Should insert a new model', async () => {
+      const result = await sut.insert(model);
+      expect(result).toStrictEqual(sut.items[0]);
+    });
+  });
+
+  describe('Find By Id', () => {
+    it('Should throw not found error', async () => {
+      const id = randomUUID();
+      await expect(sut.findById(id)).rejects.toThrow(
+        new NotFoundError(`Model not found using id ${id}`),
+      );
+    });
+    it('Should find a model by id', async () => {
+      const data = await sut.insert(model);
+      const result = await sut.findById(data.id);
+      expect(result).toStrictEqual(data);
+      expect(result.name).toStrictEqual(data.name);
+      expect(result.id).toStrictEqual(data.id);
+    });
+  });
+
+  describe('Update an model', () => {
+    it('Should throw not found error for update', async () => {
+      await expect(sut.update(model)).rejects.toThrow(
+        new NotFoundError(`Model not found using id ${model.id}`),
+      );
+    });
+
+    it('Should update a model', async () => {
+      const data = await sut.insert(model);
+      const updatedModel = {
+        id: data.id,
+        name: 'test 2',
+        price: 101,
+        created_at,
+        updated_at,
+      };
+      const result = await sut.update(updatedModel);
+      expect(result.id).toStrictEqual(data.id);
+      expect(result.name).toStrictEqual('test 2');
+    });
+  });
+
+  describe('Delete', () => {
+    it('Should throw not found error for delete', async () => {
+      const id = randomUUID();
+      await expect(sut.delete(id)).rejects.toThrow(
+        new NotFoundError(`Model not found using id ${id}`),
+      );
+    });
+
+    it('Should delete a model by id', async () => {
+      const data = await sut.insert(model);
+      await sut.delete(data.id);
+      expect(sut.items.length).toBe(0);
+    });
+  });
+
+  describe('Apply filter', () => {
+    it('Should no filter items', async () => {
+      const items = [model];
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any);
+      const result = await sut['applyFilter'](items, null);
+      expect(spyFilterMethod).not.toHaveBeenCalled();
+      expect(result).toStrictEqual(items);
+    });
+
+    it('Should filter items using filter params', async () => {
+      const items = [
+        { id: randomUUID(), name: 'TEST', price: 20, created_at, updated_at },
+        { id: randomUUID(), name: 'test', price: 20, created_at, updated_at },
+        { id: randomUUID(), name: 'Other', price: 30, created_at, updated_at },
+      ];
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any);
+      let result = await sut['applyFilter'](items, 'TEST');
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1);
+      expect(result).toStrictEqual([items[0], items[1]]);
+
+      result = await sut['applyFilter'](items, 'test');
+      expect(result).toStrictEqual([items[0], items[1]]);
+
+      result = await sut['applyFilter'](items, 'Other');
+      expect(result).toStrictEqual([items[2]]);
+
+      result = await sut['applyFilter'](items, 'no-filter');
+      expect(result).toHaveLength(0);
+    });
   });
 });
