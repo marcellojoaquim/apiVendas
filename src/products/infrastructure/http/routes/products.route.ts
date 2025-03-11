fa0d020f7792cfbb086bdfa8dd2ccac106fd01af
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-03-11 17:28:20 -0300

    documentation for get product router

--- src/products/infrastructure/http/routes/products.route.ts
+++ src/products/infrastructure/http/routes/products.route.ts
@@ -77,6 +77,29 @@ const productRouter = Router();
  */
 productRouter.post('/', createProductController);
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: The product
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ */
 productRouter.get('/:id', getProductController);
 
 export { productRouter };
