c6cad7c5f6543d4d2591c89b8afa0e54bf7d0e8e
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-03-18 09:14:59 -0300

    create typeorm entity of user

---
+++ src/users/infrastructure/typeorm/entities/users.entities.ts
@@ -0,0 +1,32 @@
+import { UserModel } from '@/users/domain/models/users.model';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User implements UserModel {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('varchar')
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column('varchar')
+  avatar?: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
